289ca4bd92caf7bf7b09902ab300e0d7ee4a9540
zakisamson <zakisamson@gmail.com>
2021-03-20 21:46:49 +0700

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './navbar.css'
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <nav>
+                <div className="navbar-inner">
+                    <div className="navbar-left">
+                        <ul id="navlist-left" type="none">
+                            <li style={{ width:'10%'}}>
+                                <img src="https://cdn.freelogovectors.net/wp-content/uploads/2018/11/jwplayer-logo.png" width="115%"></img></li>
+                            <li>Products</li>
+                            <li>Advertisers</li>
+                            <li>Solutions</li>
+                            <li>Resources</li>
+                            <li>Company</li>
+                        </ul>
+                    </div>
+                    <div className="navbar-right">
+                    <ul id="navlist-right" type="none">
+                            <li>Support</li>
+                            <li>Dashboard</li>
+                            <li><a id="btn-cta">Pricing</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+         );
+    }
+}
+ 
+export default Navbar;
